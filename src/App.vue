b4e33df04416df7295a60a8188fa4ecc29917d7f
lekealex <106382948+lekealex@users.noreply.github.com>
2022-10-10 09:09:02 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">BiblioInfo</RouterLink>
+      <RouterLink to="/cadlivros">Cadastro de Livros</RouterLink>
+      <RouterLink to="/cadautores">Cadastro de Autores</RouterLink>
+      <RouterLink to="/cadeditoras">Cadastro de Editoras</RouterLink>
+      <RouterLink to="/cadcategorias">Cadastro de Categorias</RouterLink>
+      
+    </nav> 
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+header {
+  line-height: 5.5;
+  max-height: 100vh;
+  margin-bottom: 5vh;
+  padding: 1vh;
+  box-shadow: 1px 1px 400px rgb(25, 0, 255);
+  font-family: 'Gill Sans', 'sans-serif';
+}
+
+a, .green {
+  text-decoration: none;
+  color: rgb(19, 17, 184);
+  transition: 0.5s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: rgb(78, 76, 146);
+  }
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: rgb(78, 76, 146);
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 6rem;
+  border-left: 1px solid var(--color-border);
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+<h1 class="Título">
+  Livraria
+</h1>
+    <h3 class="Home">Livraria para o projeto integrador.(HomeView)</h3>
+</template>
+<style>
+.Título{
+  text-align: center;
+  font-family: 'Gill Sans', 'sans-serif';
+} 
+
+.Home{
+  color: rgb(19, 17, 184);
+  text-align: center;
+  margin-left: 35%;
+  margin-right: 35%;
+  margin-top: 3%;
+  font-family: 'Gill Sans', 'sans-serif';
+}
+</style>
